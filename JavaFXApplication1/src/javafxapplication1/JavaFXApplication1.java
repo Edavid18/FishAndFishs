0c2d5cd58ff1298f98ec5295eb0d15632bf95435
Davllld <eliasdavidvidal24@gmail.com>
2024-11-14 15:25:37 -0500

    I created a test branch

--- JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
+++ JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
@@ -26,7 +26,7 @@ public class JavaFXApplication1 extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Hello World in the test branch!");
             }
         });
         
